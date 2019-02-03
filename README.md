3ab254b5f24aa04158350444bb0b0ee2811dd91b
Chmbrs <juanjosechambers@gmail.com>
2019-02-03 10:30:22 -0200

    First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# YouTube Music RESTful API
+
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+A simple RESTful API connected with YouTube API.
+
+## Motivation
+This is meant to be a hands-on learning project for anyone who wants to understand how RESTful API's works and their potential.
+
+## Screenshots
+
+
+## Features
+- Saves the music videos information in a relational database.
+- Gets the last 50 of your YouTube liked **music** videos.
+- Add and delete videos.
+
+## Prerequisites
+For gathering the videos from the auto-generated liked music from YouTube, you'll need a `client_id.json` file with the credentials from Google.
+
+To get the file:
+1. Use this [wizard](https://console.developers.google.com/flows/enableapi?apiid=youtube) to create or select a project in the Google Developers Console and automatically turn on the API. Click **Continue**, then **Go to credentials**.
+2. On the **Add credentials to your project page**, click the **Cancel** button.
+3. At the top of the page, select the **OAuth consent screen** tab. Select an **Email address**, enter a **Product name** if not already set, and click the **Save** button.
+4. Select the **Credentials** tab, click the Create credentials button and select **OAuth client ID**.
+5. Select the application type **Web application** and enter the name "YouTube Data API". Under **Authorized redirect URIs**, add `http://localhost:8090/oauth2callback`, which is the default redirect URL used in the code.
+
+You can specify a different redirect URI. If you do, then you may also need to change the sample code to launch the web server at an address other than `http://localhost:8090` and/or to specify a callback endpoint other than `oauth2callback`.
+6. Click the **Create** button.
+7. Click the download icon (Download JSON) button to the right of the client ID.
+8. Move the downloaded file `client_id.json` to your working directory.
+
+## Installation
+
+This project uses `Pipenv` for managing the dependencies.
+
+
+## License: MIT
+Copyright (c) 2019 Juan José Chambers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+MIT © [Juan José Chambers](https://github.com/chmbrs/)
---
+++ app.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+import os
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+import flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import Api, Namespace, Resource, fields
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+
+################################################################################
+################################################################################
+#Google API BEGIN
+################################################################################
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "client_id.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+################################################################################
+#Google API END
+################################################################################
+################################################################################
+
+# Extensions initialization
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Note: A secret key is included in the sample so that it works, but if you
+# use this code in your application please replace this with a truly secret
+# key. See http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
+app.secret_key = 'Super Secret Key'
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+api = Api(app, version='1.0', title='YouTube Music Videos API',
+          description='An API for your YouTube Music Videos')
+
+# Database table definition (SQLAlchemy)
+class Vid(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    code = db.Column(db.String())
+    def link(self):
+        return f'https://www.youtube.com/watch?v={self.code}'
+
+# Serialization/Deserialization schema definition
+class VidSchema(ma.ModelSchema):
+    class Meta:
+        strict = True
+        model = Vid()
+        fields = ['id', 'title', 'link']
+
+vid_schema = VidSchema()
+vids_schema = VidSchema(many=True)
+
+#API model
+video_model = api.model('videos', {
+    'title': fields.String('Title of the video.'),
+    'code': fields.String('Code of the video.')
+})
+
+# "Videos" resource RESTful API definitions
+videos_api = Namespace('videos')
+api.add_namespace(videos_api)
+
+# endpoint to create new video
+@videos_api.route('/add')
+class AddVideos(Resource):
+    @videos_api.expect(video_model)
+    def post(self):
+        """
+        Add new video to the list
+        """
+        title = videos_api.payload['title']
+        code = videos_api.payload['code']
+
+        new_video = Vid(title=title, code=code)
+
+        db.session.add(new_video)
+        db.session.commit()
+
+        return {'result': 'video added'}, 201
+
+
+@videos_api.route('/')
+class VideosList(Resource):
+
+    def get(self):
+        """
+        Get all your stored music videos
+        """
+        videos_all = Vid.query.all()
+        output = vids_schema.dump(videos_all).data
+        return {'videos':output}
+
+    # @app.route('/youtube')
+    # def get(self):
+    #     """
+    #     Get all your liked music YouTube videos
+    #     (Requires authentication)
+    #     """
+    #     pass
+
+
+@videos_api.route('/youtube')
+class YoutubeLikedVideos(Resource):
+    def get(self):
+        """
+        Get the last 50 YouTube liked music videos
+        Note: The session must be authorized for retrieving the list.
+        To launch OAuth2 please visit http://localhost:8090/videos/youtube in your browser once.
+        """
+        if 'credentials' not in flask.session:
+            return flask.redirect('authorize')\
+        # Load the credentials from the session.
+        credentials = google.oauth2.credentials.Credentials(**flask.session['credentials'])
+        client = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+        response =  playlist_items_list_by_playlist_id(client,part='snippet', maxResults=50, playlistId='LM')
+
+        return response
+
+
+
+################################################################################
+################################################################################
+#Google API BEGIN
+################################################################################
+@app.route('/authorize')
+def authorize():
+  # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
+  # steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  authorization_url, state = flow.authorization_url(
+      # This parameter enables offline access which gives your application
+      # both an access and refresh token.
+      access_type='offline',
+      # This parameter enables incremental auth.
+      include_granted_scopes='true')
+
+  # Store the state in the session so that the callback can verify that
+  # the authorization server response.
+  flask.session['state'] = state
+
+  return flask.redirect(authorization_url)
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verify the authorization server response.
+  state = flask.session['state']
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = flask.request.url
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store the credentials in the session.
+  # ACTION ITEM for developers:
+  #     Store user's access and refresh tokens in your data store if
+  #     incorporating this code into your real app.
+  credentials = flow.credentials
+  flask.session['credentials'] = {
+      'token': credentials.token,
+      'refresh_token': credentials.refresh_token,
+      'token_uri': credentials.token_uri,
+      'client_id': credentials.client_id,
+      'client_secret': credentials.client_secret,
+      'scopes': credentials.scopes
+  }
+
+  return flask.redirect('videos/youtube')
+
+#Response functions
+
+# This function extracts the desired values from the YouTube API response
+def parse_response(response):
+    if response:
+        videos = []
+        for video in response['items']:
+            title = video['snippet']['title']
+            code = video['snippet']['resourceId']['videoId']
+            link = f'https://www.youtube.com/watch?v={code}'
+            videos.append({'title': title, 'code': code, 'link':link})
+
+        return videos
+    else:
+      return ('This request does not return a response. For these samples, ' +
+              'this is generally true for requests that delete resources, ' +
+              'such as <code>playlists.delete()</code>, but it is also ' +
+              'true for some other methods, such as <code>videos.rate()</code>.')
+
+def print_response(response):
+  if response:
+      a = flask.jsonify(**response)
+      return a['items'][0]
+  else:
+    return ('This request does not return a response. For these samples, ' +
+            'this is generally true for requests that delete resources, ' +
+            'such as <code>playlists.delete()</code>, but it is also ' +
+            'true for some other methods, such as <code>videos.rate()</code>.')
+
+# Remove keyword arguments that are not set
+def remove_empty_kwargs(**kwargs):
+  good_kwargs = {}
+  if kwargs is not None:
+    for key, value in kwargs.items():
+      if value:
+        good_kwargs[key] = value
+  return good_kwargs
+
+def playlist_items_list_by_playlist_id(client, **kwargs):
+  kwargs = remove_empty_kwargs(**kwargs)
+
+  response = client.playlistItems().list(
+    **kwargs
+  ).execute()
+
+  # return print_response(response)
+  return parse_response(response)
+
+################################################################################
+#Google API END
+################################################################################
+################################################################################
+
+
+
+if __name__ == '__main__':
+  # When running locally, disable OAuthlib's HTTPs verification. When
+  # running in production *do not* leave this option enabled.
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+  db.create_all()
+
+  with db.session.begin(nested=True):
+    db.session.add(Vid(title = 'Boris Brejcha - I Take It Smart', code = 'XA4vo1kef6g'))
+    db.session.add(Vid(title = 'Noku Mana - Curawaka', code = 'DU64jmOPL5k'))
+
+  app.run('localhost', 8090, debug=True,) #use_reloader=False)
